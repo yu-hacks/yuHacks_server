bb56308ae9edf6c10fa207c8511b8757e3ab2887
Abdella <abudy72@my.yorku.ca>
2023-07-07 19:48:33 -0400

    added Mongo service module

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,12 +2,9 @@ import dotenv from 'dotenv'
 
 // Set NODE_ENV=Prod when on production to use system variables.
 let path;
-switch (process.env.NODE_ENV) {
-  case "dev":
-    path = `${__dirname}/../../.env.development`;
-    break;
-}
+  path = `${__dirname}/../../.env.development`;
 
 dotenv.config({ path: path });
 export const publicKey: string = process.env.publicKey ? process.env.publicKey: " "
 export const privateKey: string = process.env.privateKey ? process.env.privateKey: " "
+export const dbURI: string = process.env.dbURI ? process.env.dbURI: " "

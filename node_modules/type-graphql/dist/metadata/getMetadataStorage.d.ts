a1922b4e45e44f3a39817719d471454da2728d57
Aradhya <aradhya@my.yorku.ca>
2023-07-11 23:41:00 -0400

    Latest

--- node_modules/type-graphql/dist/metadata/getMetadataStorage.d.ts
+++
@@ -1,5 +0,0 @@
-import { MetadataStorage } from "../metadata/metadata-storage";
-declare global {
-    var TypeGraphQLMetadataStorage: MetadataStorage;
-}
-export declare function getMetadataStorage(): MetadataStorage;

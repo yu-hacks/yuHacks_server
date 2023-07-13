0aec269a17649991d244c02fd898328ea11b7e24
Aradhya <aradhya@my.yorku.ca>
2023-07-12 23:30:38 -0400

    gql

---
+++ src/db/index.ts
@@ -0,0 +1,19 @@
+import { dbURI } from "../utils/config";
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const uri = dbURI;
+    console.log(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

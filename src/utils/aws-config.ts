831a1f31693238e9f2f074602d7ef649d1825377
Yienisha Abeyratne <97560226+yienishaa@users.noreply.github.com>
2023-07-17 13:27:54 -0400

    Update aws-config.ts

--- src/utils/aws-config.ts
+++ src/utils/aws-config.ts
@@ -3,8 +3,8 @@ import AWS from "aws-sdk";
 const dotenv = require('dotenv');
 
 export const s3 = new AWS.S3({
-    accessKeyId: "AKIA4X3ETPYDJB7J34L4",
-    secretAccessKey: "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",
+    accessKeyId: "<key>",
+    secretAccessKey: "<key>",
     region: "us-east-1"
 });
 
